60cfaeee99f2c8780993aac0d23f04b003feddd1
Tharindu <tharindhu@redberylit.com>
2020-09-11 13:31:56 +0530

    base url change

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://cas.rbdemo.mili/';
+$config['base_url'] = 'http://rbspace.rbdemo.live/';
 //$config['base_url'] = '';
 
 
